14f36446acf94224f35167e5dd46284a060eda16
dpi <pro@danielph.in>
2018-03-07 10:28:34 +0800

    Restore explode utility

--- lib/Drupal/Component/Utility/Tags.php
+++ lib/Drupal/Component/Utility/Tags.php
@@ -12,6 +12,36 @@ class Tags {
   /**
    * Explodes a string of tags into an array.
    *
+   * @param string $tags
+   *   A string to explode.
+   *
+   * @return array
+   *   An array of tags.
+   */
+  public static function explode($tags) {
+    // This regexp allows the following types of user input:
+    // this, "somecompany, llc", "and ""this"" w,o.rks", foo bar
+    $regexp = '%(?:^|,\ *)("(?>[^"]*)(?>""[^"]* )*"|(?: [^",]*))%x';
+    preg_match_all($regexp, $tags, $matches);
+    $typed_tags = array_unique($matches[1]);
+
+    $tags = [];
+    foreach ($typed_tags as $tag) {
+      // If a user has escaped a term (to demonstrate that it is a group,
+      // or includes a comma or quote character), we remove the escape
+      // formatting so to save the term into the database as the user intends.
+      $tag = trim(str_replace('""', '"', preg_replace('/^"(.*)"$/', '\1', $tag)));
+      if ($tag != "") {
+        $tags[] = $tag;
+      }
+    }
+
+    return $tags;
+  }
+
+  /**
+   * Explodes a string of tags into an array.
+   *
    * Best effort is made to get tags, errors will be placed into an array if
    * string is invalid.
    *
