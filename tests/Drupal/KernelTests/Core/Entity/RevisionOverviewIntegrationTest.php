abd916b6ddce04317e21d0707f00cf59369bc2fb
acbramley <nobody@example.com>
2019-07-06 20:43:38 +0800

    119: https://www.drupal.org/files/issues/2020-07-06/2350939-119-generic-revision-ui.patch

--- tests/Drupal/KernelTests/Core/Entity/RevisionOverviewIntegrationTest.php
+++ tests/Drupal/KernelTests/Core/Entity/RevisionOverviewIntegrationTest.php
@@ -23,7 +23,7 @@ class RevisionOverviewIntegrationTest extends KernelTestBase {
   /**
    * {@inheritdoc}
    */
-  protected function setUp() {
+  protected function setUp(): void {
     parent::setUp();
 
     \Drupal::service('router.builder')->rebuild();
